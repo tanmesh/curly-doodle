bba15640a625ae8c3204341a461432f1c414c2a2
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-06-17 22:09:44 +0530

    Sieve of Eratosthenes

--- standard problems/Sieve of Eratosthenes.cpp
+++ standard problems/Sieve of Eratosthenes.cpp
@@ -13,19 +13,23 @@ using namespace std;
 
 vector<int> sieveOfEratosthenes(int n) {
 	vector<int> prime;
-	vector<bool> isPrime(n, false);
+	vector<bool> isPrime(n + 1, true);
 
-	for (int i = 2; i <= n; ++i) {
-		if (isPrime[i] == false) {
-			isPrime[i] = true;
-			prime.push_back(i);
-
-			for (int j = i + i; j <= n; j += i) {
-				isPrime[j] = true;
+	isPrime[0] = false;
+	isPrime[1] = false;
+	for (int i = 2; i * i <= n; ++i) {
+		if (isPrime[i] == true) {
+			for (int j = i * i; j <= n; j += i) {
+				isPrime[j] = false;
 			}
 		}
 	}
 
+	for (int i = 0; i <= n; ++i) {
+		if (isPrime[i]) {
+			prime.push_back(i);
+		}
+	}
 	return prime;
 }
 
