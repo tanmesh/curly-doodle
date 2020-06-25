470c7cb2c181fb81e2c90b100fb6ab207c97b9bc
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-06-25 22:41:52 +0530

    https://codeforces.com/contest/1373/problem/B

---
+++ abrakadabra/01 Game.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(a)          a.begin(), a.end()
+#define allr(a)         a.rbegin(), a.rend()
+#define F				first
+#define S				second
+#define pb              push_back
+#define ll				long long
+#define fast 			ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define watch(x) 		cout << (#x) << " is " << (x) << endl
+
+void solve() {
+	string input;
+	cin >> input;
+	string ans = "NET";
+
+	stack<char> stk;
+	stk.push(input[0]);
+	for (int i = 1; i < input.size(); ++i) {
+		if (!stk.empty() && input[i] != stk.top()) {
+			stk.pop();
+			if (ans == "NET") ans = "DA";
+			else ans = "NET";
+		} else {
+			stk.push(input[i]);
+		}
+	}
+	cout << ans << endl;
+}
+
+int main () {
+	fast
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
