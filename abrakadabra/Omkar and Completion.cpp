ce5947ee076a4a3454c1976205d42249a0e9cd03
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2020-07-11 23:23:29 +0530

    https://codeforces.com/contest/1372/problem/A

---
+++ abrakadabra/Omkar and Completion.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(a)          a.begin(), a.end()
+#define allr(a)         a.rbegin(), a.rend()
+#define F				first
+#define S				second
+#define pb              push_back
+#define ll				long long
+#define fast 			ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define watch(x) 		cout << (#x) << " is " << (x) << endl
+
+void solve() {
+	ll n;
+	cin >> n;
+	while (n--) {
+		cout << 1 << " ";
+	}
+	cout << endl;
+}
+
+int main () {
+	fast
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
