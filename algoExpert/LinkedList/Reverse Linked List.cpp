9e797d3afbaa1b032d12e482ea16417acab5975b
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2021-01-10 23:13:37 +0530

    update

--- algoExpert/LinkedList/Reverse Linked List.cpp
+++ algoExpert/LinkedList/Reverse Linked List.cpp
@@ -1,30 +1,59 @@
+// // My solution
+// using namespace std;
+
+// class LinkedList {
+// public:
+//   int value;
+//   LinkedList *next;
+
+//   LinkedList(int value) {
+//     this->value = value;
+//     this->next = NULL;
+//   }
+// };
+
+// LinkedList *reverseLinkedList(LinkedList *head) {
+//   LinkedList *T1 = NULL;
+// 	LinkedList *T2 = head;
+// 	LinkedList *T3 = head->next;
+
+// 	while(T3 != NULL) {
+// 		T2->next = T1;
+// 		T1 = T2;
+// 		T2 = T3;
+// 		T3 = T3->next;
+// 	}
+
+// 	T2->next = T1;
+
+//   return T2;
+// }
+
+// Clean Solution
 using namespace std;
 
 class LinkedList {
 public:
-  int value;
-  LinkedList *next;
+	int value;
+	LinkedList *next;
 
-  LinkedList(int value) {
-    this->value = value;
-    this->next = NULL;
-  }
+	LinkedList(int value) {
+		this->value = value;
+		this->next = NULL;
+	}
 };
 
+// Time O(n), Space O(1)
 LinkedList *reverseLinkedList(LinkedList *head) {
-  LinkedList *T1 = NULL;
-	LinkedList *T2 = head;
-	LinkedList *T3 = head->next;
-	
-	while(T3 != NULL) {
-		T2->next = T1;
-		T1 = T2;
-		T2 = T3;
-		T3 = T3->next;
-	}
-	
-	T2->next = T1;
-	
-  return T2;
-}
+	LinkedList *previousNode = NULL;
+	LinkedList *currentNode = head;
 
+	while (currentNode != NULL) {
+		LinkedList *nextNode = currentNode->next;
+		currentNode->next = previousNode;
+		previousNode = currentNode;
+		currentNode = nextNode;
+	}
+
+	return previousNode;
+}
