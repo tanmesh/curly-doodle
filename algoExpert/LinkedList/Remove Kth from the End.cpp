0ecdeaf0337340d269d9fe120b529fea93eff3b0
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2021-01-10 22:46:34 +0530

    updated

--- algoExpert/LinkedList/Remove Kth from the End.cpp
+++ algoExpert/LinkedList/Remove Kth from the End.cpp
@@ -1,3 +1,47 @@
+// // Solution 1
+
+// #include <vector>
+// using namespace std;
+
+// class LinkedList {
+// public:
+// 	int value;
+// 	LinkedList *next;
+
+// 	LinkedList(int value);
+// 	void addMany(vector<int> values);
+// 	vector<int> getNodesInArray();
+// };
+
+// void removeKthNodeFromEnd(LinkedList *head, int k) {
+// 	int len = 0;
+// 	LinkedList* tmp = head;
+
+// 	// calculate list length and store in "len"
+// 	while (tmp != NULL) {
+// 		++len;
+// 		tmp = tmp -> next;
+// 	}
+
+// 	// traverse from front
+// 	k = len - k;
+
+// 	if (k == 0) {
+// 		head -> value = head -> next -> value;
+// 		head -> next = head -> next -> next;
+// 		return;
+// 	}
+
+// 	tmp = head;
+// 	while (tmp -> next != NULL && k > 1) {
+// 		--k;
+// 		tmp = tmp -> next;
+// 	}
+// 	tmp -> next = tmp -> next -> next;
+// }
+
+
+// // Solution 2
 #include <vector>
 using namespace std;
 
@@ -12,29 +56,26 @@ public:
 };
 
 void removeKthNodeFromEnd(LinkedList *head, int k) {
-	int len = 0;
-	LinkedList* tmp = head;
+	LinkedList *first = head;
+	LinkedList *second = head;
+	int cnt = k;
 
-	// calculate list length and store in "len"
-	while (tmp != NULL) {
-		++len;
-		tmp = tmp -> next;
+	// move second ptr K nodes ahead
+	while (second != NULL && cnt > 0) {
+		second = second -> next;
+		--cnt;
 	}
 
-	// traverse from front
-	k = len - k;
-
-	if (k == 0) {
+	if (second == NULL) {
 		head -> value = head -> next -> value;
 		head -> next = head -> next -> next;
 		return;
 	}
 
-	tmp = head;
-	while (tmp -> next != NULL && k > 1) {
-		--k;
-		tmp = tmp -> next;
+	// move both first and second ptr till NULL
+	while (second->next != NULL) {
+		second = second -> next;
+		first = first -> next;
 	}
-	tmp -> next = tmp -> next -> next;
+	first -> next = first -> next -> next;
 }
-
