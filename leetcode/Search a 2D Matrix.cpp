4fc01ae62d4e1f27808cf89719a992173585908d
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2021-01-28 16:39:37 +0530

    28Jan21

---
+++ leetcode/Search a 2D Matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int m = matrix.size(), n = matrix[0].size();
+
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midElement = matrix[mid / n][mid % n];
+
+            if (target == midElement) {
+                return true;
+            }
+            if (target < midElement) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
