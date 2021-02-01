88459201f1e28fa7abf5f6977d0d680badd889d7
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2021-02-01 15:07:46 +0530

    1Feb21

---
+++ algoExpert/Graph/Breadth-first Search.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+using namespace std;
+
+// Do not edit the class below except
+// for the breadthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+public:
+	string name;
+	vector<Node *> children;
+
+	Node(string str) { name = str; }
+
+	// O(V+E)T, O(V)S
+	vector<string> breadthFirstSearch(vector<string> *array) {
+		queue<Node *> q;
+		q.push(this);
+
+		while (!q.empty()) {
+			Node current = *q.front();
+			q.pop();
+
+			array->push_back(current.name);
+			for (int i = 0; i < current.children.size(); ++i) {
+				q.push(current.children[i]);
+			}
+		}
+
+		return *array;
+	}
+
+	Node *addChild(string name) {
+		Node *child = new Node(name);
+		children.push_back(child);
+		return this;
+	}
+};
+
