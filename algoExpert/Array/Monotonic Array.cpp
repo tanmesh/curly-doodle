516989b84ad2aa88429905bb5cc20d321727771f
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2021-02-02 11:38:02 +0530

    2Feb21

--- algoExpert/Array/Monotonic Array.cpp
+++ algoExpert/Array/Monotonic Array.cpp
@@ -1,30 +1,49 @@
+// Solution 3
 using namespace std;
 
-bool breakDir(int dir, int prevInt, int currInt) {
-	int diff = currInt - prevInt;
-	if (dir > 0) {
-		return diff < 0;
-	}
-	return diff > 0;
-}
-
 bool isMonotonic(vector<int> array) {
-	if (array.size() <= 2) {
-		return true;
-	}
+	bool isNonDecr = true;
+	bool isNonIncr = true;
 
-	int dir = array[1] - array[0];
-
-	for (int i = 2; i < array.size(); ++i) {
-		if (dir == 0) {
-			dir = array[i] - array[i - 1];
-			continue;
+	for (int i = 1; i < array.size(); ++i) {
+		if (array[i - 1] < array[i]) {
+			isNonDecr = false;
 		}
-
-		if (breakDir(dir, array[i - 1], array[i])) {
-			return false;
+		if (array[i - 1] > array[i]) {
+			isNonIncr = false;
 		}
 	}
-	return true;
+	return isNonIncr || isNonDecr;
 }
+
+// // Solution 1
+// using namespace std;
+
+// bool breakDir(int dir, int prevInt, int currInt) {
+// 	int diff = currInt - prevInt;
+// 	if (dir > 0) {
+// 		return diff < 0;
+// 	}
+// 	return diff > 0;
+// }
+
+// bool isMonotonic(vector<int> array) {
+// 	if (array.size() <= 2) {
+// 		return true;
+// 	}
+
+// 	int dir = array[1] - array[0];
+
+// 	for (int i = 2; i < array.size(); ++i) {
+// 		if (dir == 0) {
+// 			dir = array[i] - array[i - 1];
+// 			continue;
+// 		}
+
+// 		if (breakDir(dir, array[i - 1], array[i])) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
 
