4387e84892eff0cf32cb624dcaedc5e1b0f2b49e
Tanmesh Mishra <tanmeshmishra@Tanmeshs-MacBook-Pro.local>
2021-02-17 11:40:27 +0530

    17Feb21

--- algoExpert/Sorting/Bubble Sort.cpp
+++ algoExpert/Sorting/Bubble Sort.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 vector<int> bubbleSort(vector<int> array) {
 	for (int i = 0; i < array.size(); ++i) {
+		bool atleastOneSwap = false;
 		for (int j = 0; j + 1 < array.size() - i; ++j) {
 			if (array[j] > array[j + 1]) {
+				atleastOneSwap = true;
 				int tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 			}
+		}
+		if (atleastOneSwap == false) {
+			return array;
 		}
 	}
 
