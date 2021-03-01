6ca343d36902b7d0e6542ffb1ad04dfc0f2bf548
Tanmesh Narayan Mishra <tanmeshnm@Tanmeshs-MacBook-Pro.local>
2021-03-01 12:06:01 +0530

    1 March 21

--- leetcode/Is Subsequence.cpp
+++ leetcode/Is Subsequence.cpp
@@ -1,18 +1,31 @@
-// Solution 1: using 2 ptr, O(leght of T)T, O(1)S
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int leftBound = s.size(), rightBound = t.size();
-        int leftPtr = 0, rightPtr = 0;
-        while (leftPtr < leftBound && rightPtr < rightBound) {
-            if (s[leftPtr] == t[rightPtr]) {
-                ++leftPtr;
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()) {
+            if(s[i] == t[j]) {
+                ++i;
             }
-            ++rightPtr;
+            ++j;
         }
-        if (leftPtr == leftBound) {
-            return true;
-        }
-        return false;
+        return (i == s.length() && j <= t.length());
     }
 };
+// // Solution 1: using 2 ptr, O(leght of T)T, O(1)S
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int leftBound = s.size(), rightBound = t.size();
+//         int leftPtr = 0, rightPtr = 0;
+//         while (leftPtr < leftBound && rightPtr < rightBound) {
+//             if (s[leftPtr] == t[rightPtr]) {
+//                 ++leftPtr;
+//             }
+//             ++rightPtr;
+//         }
+//         if (leftPtr == leftBound) {
+//             return true;
+//         }
+//         return false;
+//     }
+// };
