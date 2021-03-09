3a03a97939cd155f746890ad16cfa43072d25419
Tanmesh <tanmeshnm@Tanmeshs-MacBook-Pro.local>
2021-03-09 10:27:24 +0530

    9 Mar 21

--- leetcode/Basic Calculator II.cpp
+++ leetcode/Basic Calculator II.cpp
@@ -4,10 +4,12 @@ public:
         if(s.length() == 0) {
             return 0;
         }
-        stack<int> stack;
-        int currentNum = 0;
-        char currentOperator = '+';
         
+        int currentNum = 0;
+        int result = 0;
+        int lastNum = 0;
+        
+        char currentOperator = '+';
         for(int i=0; i<s.length(); ++i) {
             char currentChar = s[i];
             if(isdigit(currentChar)) {
@@ -15,29 +17,65 @@ public:
             }
             if(!isdigit(currentChar) && currentChar != ' ' || i == s.length() - 1) {
                 if(currentOperator == '-') {
-                    stack.push(-currentNum);
+                    result = result + lastNum;
+                    lastNum = currentNum * -1;
                 } else if(currentOperator == '+') {
-                    stack.push(currentNum);
+                    result = result + lastNum;
+                    lastNum = currentNum;
                 } else if(currentOperator == '*') {
-                    int stackTop = stack.top();
-                    stack.pop();
-                    stack.push(stackTop * currentNum);
+                    lastNum = lastNum * currentNum;
                 } else if(currentOperator == '/') {
-                    int stackTop = stack.top();
-                    stack.pop();
-                    stack.push(stackTop / currentNum);
+                    lastNum = lastNum / currentNum;
                 }
                 currentOperator = currentChar;
                 currentNum = 0;
             }
         }
         
-        int result = 0;
-        while(!stack.empty()) {
-            result += stack.top();
-            stack.pop();
-        }
-        
-        return result;
+        return result + lastNum;
     }
 };
+
+// class Solution {
+// public:
+//     int calculate(string s) {
+//         if(s.length() == 0) {
+//             return 0;
+//         }
+//         stack<int> stack;
+//         int currentNum = 0;
+//         char currentOperator = '+';
+        
+//         for(int i=0; i<s.length(); ++i) {
+//             char currentChar = s[i];
+//             if(isdigit(currentChar)) {
+//                 currentNum = currentNum*10 + (currentChar - '0');
+//             }
+//             if(!isdigit(currentChar) && currentChar != ' ' || i == s.length() - 1) {
+//                 if(currentOperator == '-') {
+//                     stack.push(-currentNum);
+//                 } else if(currentOperator == '+') {
+//                     stack.push(currentNum);
+//                 } else if(currentOperator == '*') {
+//                     int stackTop = stack.top();
+//                     stack.pop();
+//                     stack.push(stackTop * currentNum);
+//                 } else if(currentOperator == '/') {
+//                     int stackTop = stack.top();
+//                     stack.pop();
+//                     stack.push(stackTop / currentNum);
+//                 }
+//                 currentOperator = currentChar;
+//                 currentNum = 0;
+//             }
+//         }
+        
+//         int result = 0;
+//         while(!stack.empty()) {
+//             result += stack.top();
+//             stack.pop();
+//         }
+        
+//         return result;
+//     }
+// };
