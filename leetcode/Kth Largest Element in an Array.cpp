1975d07959d054e627bbe1f20fb448b7fad9783f
Tanmesh <tanmeshnm@Tanmeshs-MacBook-Pro.local>
2021-03-12 14:32:58 +0530

    12 Mar 21

--- leetcode/Kth Largest Element in an Array.cpp
+++ leetcode/Kth Largest Element in an Array.cpp
@@ -18,67 +18,38 @@ public:
 		return pq.top();
 	}
 };
+
 // // Solution 1: using quickselect
-// #include <bits/stdc++.h>
-
-// using namespace std;
-
-// #define all(a)          a.begin(), a.end()
-// #define allr(a)         a.rbegin(), a.rend()
-// #define F				first
-// #define S				second
-// #define pb              push_back
-// #define ll				long long
-// #define fast 			ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-// #define watch(x) 		cout << (#x) << " is " << (x) << endl
-
-// // https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/
-
 // class Solution {
+//     int partition(vector<int> &nums, int low, int high) {
+//         int pivot = low;
+//         int leftHandSide = low + 1;
+//         int rightHandSide = low + 1;
+        
+//         while(rightHandSide <= high) {
+//             if(nums[rightHandSide] < nums[pivot]) {
+//                 swap(nums[leftHandSide], nums[rightHandSide]);
+//                 ++leftHandSide;
+//             }
+//             ++rightHandSide;
+//         }
+//         swap(nums[pivot], nums[leftHandSide - 1]);
+//         return leftHandSide - 1;
+//     }
 // public:
-// 	int partition(vector<int>& input, int l, int r) {
-// 		int pivot = l;
-// 		int j = pivot + 1;
-// 		int i = j;
-// 		while (j <= r) {
-// 			if (input[pivot] > input[j]) {
-// 				swap(input[i], input[j]);
-// 				++i;
-// 			}
-// 			++j;
-// 		}
-// 		swap(input[pivot], input[i - 1]);
-// 		return i - 1;
-// 	}
-// 	int findKthLargest(vector<int>& nums, int k) {
-// 		int n = nums.size();
-// 		k = n - k;
-// 		int low = 0, high = n - 1;
-// 		while (true) {
-// 			int pivot = partition(nums, low, high);
-// 			if (pivot == k) {
-// 				return nums[pivot];
-// 			} else if (pivot < k) {
-// 				low = pivot + 1;
-// 			} else {
-// 				high = pivot - 1;
-// 			}
-// 		}
-// 	}
+//     int findKthLargest(vector<int>& nums, int k) {
+//         int low = 0, high = nums.size() - 1;
+//         k = nums.size() - k;
+//         while(true) {
+//             int pivot = partition(nums, low, high);
+//             if(pivot == k) {
+//                 return nums[k];
+//             } else if(pivot < k) {
+//                 low = pivot + 1;
+//             } else {
+//                 high = pivot - 1;
+//             }
+//         }
+//         return -1;
+//     }
 // };
-
-// int main () {
-// 	fast
-// 	Solution s = Solution();
-
-// 	int n;
-// 	cin >> n;
-// 	vector<int> input(n);
-// 	for (int i = 0; i < n; ++i) {
-// 		cin >> input[i];
-// 	}
-// 	int k;
-// 	cin >> k;
-// 	cout << s.findKthLargest(input, k) << endl;
-// 	return 0;
-// }
