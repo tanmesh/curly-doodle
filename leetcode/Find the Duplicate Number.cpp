bae63fad113701a1292e12b554124fcb5708154a
Tanmesh <tanmeshnm@Tanmeshs-MacBook-Pro.local>
2021-03-12 19:31:14 +0530

    12 Mar 21

--- leetcode/Find the Duplicate Number.cpp
+++ leetcode/Find the Duplicate Number.cpp
@@ -1,19 +1,45 @@
-// Solution 2: using sort, O(NLogN)T, O(1)S
+// SOlution 3: using linkined list cycle detection O(N)T, O(1)S
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int first = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            int second = nums[i];
-            if (first == second) {
-                return first;
-            }
-            first = second;
+        if(nums.size() < 2) {
+            return 0;
         }
-        return 0;
+        
+        int slowPtr = nums[0];
+        int fastPtr = nums[nums[0]];
+        
+        while(slowPtr != fastPtr && fastPtr < nums.size() && nums[fastPtr] < nums.size()) {
+            slowPtr = nums[slowPtr];
+            fastPtr = nums[nums[fastPtr]];
+        }
+        
+        slowPtr = 0;
+        while(slowPtr != fastPtr) {
+            slowPtr = nums[slowPtr];
+            fastPtr = nums[fastPtr];
+        }
+        
+        return fastPtr;
     }
 };
+
+// // Solution 2: using sort, O(NLogN)T, O(1)S
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int first = nums[0];
+//         for (int i = 1; i < nums.size(); ++i) {
+//             int second = nums[i];
+//             if (first == second) {
+//                 return first;
+//             }
+//             first = second;
+//         }
+//         return 0;
+//     }
+// };
 
 
 // Solution 1: using Map, O(N)T, O(N)S
